3ee27e6add376b0169182c65c2a3be7c025fa811
Dwicky Feri <dwicky.f.putra18@gmail.com>
2026-01-20 21:20:30 +0700

    feat: remove unused

--- web/src/components/layout/data/sidebar-data.ts
+++ web/src/components/layout/data/sidebar-data.ts
@@ -1,30 +1,12 @@
 import {
-  // Construction,
   LayoutDashboard,
-  // Monitor,
-  // Bug,
-  // ListTodo,
-  // FileX,
-  HelpCircle,
-  // Lock,
-  // Bell,
-  // Package,
-  // Palette,
-  // ServerOff,
   Settings,
-  // Wrench,
-  // UserCog,
-  // UserX,
-  // Users,
-  // MessagesSquare,
-  // ShieldCheck,
   Snowflake,
   Workflow,
   Command,
   Database,
   Activity,
 } from 'lucide-react'
-// import { ClerkLogo } from '@/assets/clerk-logo'
 import { type SidebarData } from '../types'
 
 export const sidebarData: SidebarData = {
@@ -49,45 +31,6 @@ export const sidebarData: SidebarData = {
           url: '/',
           icon: LayoutDashboard,
         },
-        // {
-        //   title: 'Tasks',
-        //   url: '/tasks',
-        //   icon: ListTodo,
-        // },
-        // {
-        //   title: 'Apps',
-        //   url: '/apps',
-        //   icon: Package,
-        // },
-        // {
-        //   title: 'Chats',
-        //   url: '/chats',
-        //   badge: '3',
-        //   icon: MessagesSquare,
-        // },
-        // {
-        //   title: 'Users',
-        //   url: '/users',
-        //   icon: Users,
-        // },
-        // {
-        //   title: 'Secured by Clerk',
-        //   icon: ClerkLogo,
-        //   items: [
-        //     {
-        //       title: 'Sign In',
-        //       url: '/clerk/sign-in',
-        //     },
-        //     {
-        //       title: 'Sign Up',
-        //       url: '/clerk/sign-up',
-        //     },
-        //     {
-        //       title: 'User Management',
-        //       url: '/clerk/user-management',
-        //     },
-        //   ],
-        // },
       ],
     },
     {
@@ -99,9 +42,9 @@ export const sidebarData: SidebarData = {
           url: '/sources',
         },
         {
-            title: 'Destinations',
-            icon: Snowflake,
-            url: '/destinations',
+          title: 'Destinations',
+          icon: Snowflake,
+          url: '/destinations',
         },
       ],
     },
@@ -115,68 +58,6 @@ export const sidebarData: SidebarData = {
         },
       ],
     },
-    // {
-    //   title: 'Pages',
-    //   items: [
-    //     {
-    //       title: 'Auth',
-    //       icon: ShieldCheck,
-    //       items: [
-    //         {
-    //           title: 'Sign In',
-    //           url: '/sign-in',
-    //         },
-    //         {
-    //           title: 'Sign In (2 Col)',
-    //           url: '/sign-in-2',
-    //         },
-    //         {
-    //           title: 'Sign Up',
-    //           url: '/sign-up',
-    //         },
-    //         {
-    //           title: 'Forgot Password',
-    //           url: '/forgot-password',
-    //         },
-    //         {
-    //           title: 'OTP',
-    //           url: '/otp',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'Errors',
-    //       icon: Bug,
-    //       items: [
-    //         {
-    //           title: 'Unauthorized',
-    //           url: '/errors/unauthorized',
-    //           icon: Lock,
-    //         },
-    //         {
-    //           title: 'Forbidden',
-    //           url: '/errors/forbidden',
-    //           icon: UserX,
-    //         },
-    //         {
-    //           title: 'Not Found',
-    //           url: '/errors/not-found',
-    //           icon: FileX,
-    //         },
-    //         {
-    //           title: 'Internal Server Error',
-    //           url: '/errors/internal-server-error',
-    //           icon: ServerOff,
-    //         },
-    //         {
-    //           title: 'Maintenance Error',
-    //           url: '/errors/maintenance-error',
-    //           icon: Construction,
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
     {
       title: 'Other',
       items: [
@@ -190,11 +71,6 @@ export const sidebarData: SidebarData = {
               icon: Activity,
             },
           ],
-        },
-        {
-          title: 'Help Center',
-          url: '/help-center',
-          icon: HelpCircle,
         },
       ],
     },
